6db9eb5723a1332544fb4a382a9fcd35b8ee07c9
Kofi Agyemang <ebensakyi@gmail.com>
2023-10-18 12:11:47 +0000

    _

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,6 +1,8 @@
 import Button from "@/components/Button"
+import Image from 'next/image'
 import { arrowRight } from '../../public/assets/icons'
 import { statistics } from "@/constants"
+import { bigShoe1 } from "../../public/assets/images"
 
 const Hero = () => {
   return (
@@ -11,19 +13,23 @@ const Hero = () => {
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span><br />
           <span className="text-coral-red inline-block mt-3">Nike </span>Shoes
         </h1>
-        <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life</p>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life</p>
         <Button label="Shop now" iconUrl={arrowRight}/>
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
           {
             statistics.map((stat,index)=>(
               <div key={index}>
-                <p>{stat.value}</p>
-                <p>{stat.label}</p>
+                <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+                <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
 
                 </div>
             ))
           }
         </div>
+      </div>
+
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <Image alt="shoe" src={bigShoe1} width={610} height={500} className="object-contain relative z-10"/>
       </div>
 
     </section>
